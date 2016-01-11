340916066f47ae69aa6d27b7b3e0af0deee74d48
miniz_ <anano.bodokia@azry.com>
2016-01-11 05:05:45 +0400

    FAQ page done

--- sprint1/contact-us.php
+++ sprint1/contact-us.php
@@ -83,20 +83,20 @@
     </header><!--/header-->
 
     <section id="contact-info">
-        <div class="center">                
+        <!-- <div class="center">                
             <h2>სად ვართ ჩვენ?</h2>
             <p class="lead"></p>
-        </div>
+        </div> -->
         <div class="gmap-area">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-5 text-center">
+                    <div class="col-sm-6 text-center">
                         <div class="gmap">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2974.701391131911!2d44.76494724903228!3d41.791648655188744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40446e759040b513%3A0x34c0cc349b7fad24!2sKing+Pharnavaz+Street+-+%5B2497%5D!5e0!3m2!1sen!2sge!4v1450650635996" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                         </div>
                     </div>
 
-                    <div class="col-sm-7 map-content">
+                    <div class="col-sm-6 map-content">
                         <ul class="row">
                             <li class="col-sm-6">
                                 <address>
